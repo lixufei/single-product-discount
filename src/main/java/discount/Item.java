a98ee80f700a36bd366046d71150adad8c5d6101
LiXuFei <xfli@thoughtworks.com>
2016-07-02 11:41:33 +0800

    test an item object.

---
+++ src/main/java/discount/Item.java
@@ -0,0 +1,68 @@
+package discount;
+
+import java.math.BigDecimal;
+
+public class Item {
+
+    private String barcode;
+    private String name;
+    private String unit;
+    private String category;
+    private String subCategory;
+    private BigDecimal price;
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public Item setBarcode(String barcode) {
+        this.barcode = barcode;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Item setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public Item setUnit(String unit) {
+        this.unit = unit;
+        return this;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Item setCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public Item setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Item setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+}
--- src/test/java/discount/TestDiscount.java
+++ src/test/java/discount/TestDiscount.java
@@ -2,11 +2,34 @@ package discount;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestDiscount {
     @Test
     public void shouldOnePlusOneEqualsTwo () {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void shouldGetInputItem () {
+        String barcode = "ITEM000001";
+        Item item = new Item()
+                .setBarcode(barcode)
+                .setName("可口可乐")
+                .setUnit("瓶")
+                .setCategory("食品")
+                .setSubCategory("碳酸饮料")
+                .setPrice(BigDecimal.valueOf(3.00));
+
+        assertEquals("可口可乐", item.getName());
+    }
+
+    @Test
+    public void TestBigDecimal () {
+        BigDecimal initValue = BigDecimal.ZERO;
+        initValue = initValue.add(BigDecimal.valueOf(19));
+        System.out.println("this is the init value:" + initValue);
     }
 }
